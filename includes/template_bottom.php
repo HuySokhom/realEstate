859bf33c5ebc4a9d4c82a5f9d220608a3d460336
sokhom <kom.huy@gmail.com>
2016-05-21 19:18:17 +0700

    update footer

--- includes/template_bottom.php
+++ includes/template_bottom.php
@@ -5,12 +5,18 @@
     <div class="container">
         <!-- col-md-3 -->
         <div class="col-md-3 col-sm-6">
-            <!-- About Widget -->
-            <aside class="widget widget_about">
-                <h3 class="widget-title">About Us</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-            </aside>
-            <!-- About Widget /- -->
+            <div class="col-sm-6">
+                <div class="footerbox">
+                    <h2><?php echo MODULE_CONTENT_FOOTER_CONTACT_US_HEADING_TITLE; ?></h2>
+                    <address>
+                        <strong><?php echo STORE_NAME; ?></strong><br>
+                        <?php echo nl2br(STORE_ADDRESS); ?><br>
+                        <abbr title="Phone">P:</abbr> <?php echo STORE_PHONE; ?><br>
+                        <abbr title="Email">E:</abbr> <?php echo STORE_OWNER_EMAIL_ADDRESS; ?>
+                    </address>
+                </div>
+            </div>
+
         </div><!-- col-md-3 -->
 
         <!-- col-md-3 -->
