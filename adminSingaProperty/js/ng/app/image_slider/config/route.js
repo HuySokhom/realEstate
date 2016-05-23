15e5c2576888e651f09dfb08e51b58e27d472377
John Ny <kom.huy@gmail.com>
2016-05-23 10:13:12 +0700

    update htaccess

--- adminSingaProperty/js/ng/app/image_slider/config/route.js
+++ adminSingaProperty/js/ng/app/image_slider/config/route.js
@@ -3,12 +3,12 @@ app.config([
 	'$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
 		$stateProvider.
-			state('/image_slider', {
-				url: '/image_slider',
-				templateUrl: 'js/ng/app/image_slider/partials/image_slider.html',
+			state('/', {
+				url: '/',
+				templateUrl: 'js/ng/app/image_slider/partials/image_slider.php',
                 controller: 'image_slider_ctrl'
 			})
 		;
-		$urlRouterProvider.otherwise('/image_slider');
+		$urlRouterProvider.otherwise('/');
 	}
 ]);
