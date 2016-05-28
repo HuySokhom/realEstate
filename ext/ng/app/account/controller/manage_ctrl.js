27dada540f6331516fe10bf9bb0d2eab81c10ae1
HuySokhom <kom.huy@gmail.com>
2016-05-28 21:40:22 +0700

    enable image drag and drop in tinymce

--- ext/ng/app/account/controller/manage_ctrl.js
+++ ext/ng/app/account/controller/manage_ctrl.js
@@ -91,7 +91,20 @@ app.controller(
 		$scope.add = function(){
 			tinymce.init({
 				selector:'#description',
-				plugins: 'media'
+				plugins: [
+					"advlist autolink lists link image charmap print preview hr anchor pagebreak",
+					"searchreplace wordcount visualblocks visualchars fullscreen",
+					"insertdatetime media nonbreaking save table contextmenu directionality",
+					"emoticons template paste textcolor colorpicker textpattern media"
+				],
+				toolbar1: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+				toolbar2: "print preview media | forecolor backcolor emoticons",
+				image_advtab: true,
+				templates: [
+					{ title: 'Test template 1', content: 'Test 1' },
+					{ title: 'Test template 2', content: 'Test 2' }
+				],
+				paste_data_images: true
 			});
 		};
 
