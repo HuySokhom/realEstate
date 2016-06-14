92e86f04efcd19fa3eddd6e208e535944fd5b8d9
John Ny <kom.huy@gmail.com>
2016-06-14 11:24:44 +0700

    update

--- adminSingaProperty/includes/header.php
+++ adminSingaProperty/includes/header.php
@@ -4,6 +4,10 @@
   <div class="page-sidebar">
     <!-- START X-NAVIGATION -->
     <ul class="x-navigation">
+      <li class="xn-logo">
+        <a href="index.html">Singa Property</a>
+        <a href="#" class="x-navigation-control"></a>
+      </li>
       <li class="xn-profile">
         <a href="#" class="profile-mini">
           <img src="images/icons/ico.png" alt="Logo"/>
