e070b0d5f4c6ae8dbd5f594706ba1dfb7b5ae7be
John Ny <kom.huy@gmail.com>
2016-06-15 15:57:11 +0700

    add custom css

--- adminSingaProperty/includes/template_top.php
+++ adminSingaProperty/includes/template_top.php
@@ -18,8 +18,9 @@
 <link rel="shortcut icon" href="images/icons/ico.png">
 <title><?php echo TITLE; ?></title>
 <base href="<?php echo ($request_type == 'SSL') ? HTTPS_SERVER . DIR_WS_HTTPS_ADMIN : HTTP_SERVER . DIR_WS_ADMIN; ?>" />
+<link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
 <link rel="stylesheet" type="text/css" href="css/theme-default.css">
-  <link rel="stylesheet" type="text/css" href="js/ng/lib/angular-alertify/css/alertify.css">
+<link rel="stylesheet" type="text/css" href="js/ng/lib/angular-alertify/css/alertify.css">
 </head>
 <body data-ng-app="main">
   <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
