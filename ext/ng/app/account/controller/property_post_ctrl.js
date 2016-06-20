d06a7b0d58c9269ae7d21db3e6cbe9d912f6edd8
John Ny <kom.huy@gmail.com>
2016-06-20 17:01:48 +0700

    update product post

--- ext/ng/app/account/controller/property_post_ctrl.js
+++ ext/ng/app/account/controller/property_post_ctrl.js
@@ -123,3 +123,33 @@ app.controller(
 		};
 	}
 ]);
+
+app.directive('dropZone', function() {
+	return function(scope, element, attrs) {
+		console.log(element);
+		element.dropzone({
+			url: "/upload",
+			maxFilesize: 100,
+			paramName: "uploadfile",
+			maxThumbnailFilesize: 5,
+			init: function() {
+				scope.files.push({file: 'added'}); // here works
+				this.on('success', function(file, json) {
+				});
+
+				this.on('addedfile', function(file) {
+					scope.$apply(function(){
+						alert(file);
+						scope.files.push({file: 'added'});
+					});
+				});
+
+				this.on('drop', function(file) {
+					alert('file');
+				});
+
+			}
+
+		});
+	}
+});
