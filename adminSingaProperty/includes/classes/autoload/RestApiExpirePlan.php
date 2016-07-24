992ca5ee46db51564d841248795e42e51d38d06c
HuySokhom <kom.huy@gmail.com>
2016-07-24 10:28:14 +0700

    UPDATE report expire

--- adminSingaProperty/includes/classes/autoload/RestApiExpirePlan.php
+++ adminSingaProperty/includes/classes/autoload/RestApiExpirePlan.php
@@ -3,8 +3,37 @@
 class RestApiExpirePlan extends RestApi {
 
     public function get($params){
-        $queryExpiryday = tep_db_query("select count(*) as total from customers_plan where DAY(plan_expire) = DAY(NOW()) AND MONTH(plan_expire) = MONTH(NOW())");
-        $countExpiryDay = tep_db_fetch_array($queryExpiryday);
+        $queryExpiryday = tep_db_query("
+            SELECT
+              cp.id,
+              cp.plan,
+              cp.plan_date,
+              cp.plan_expire,
+              c.user_name,
+              c.user_type,
+              c.customers_telephone,
+              c.customers_email_address
+            FROM
+              customers_plan cp INNER JOIN customers c ON cp.customers_id = c.customers_id
+            WHERE
+              cp.status = 1
+                AND
+              DAY(cp.plan_expire) = DAY(NOW())
+                AND
+            MONTH(cp.plan_expire) = MONTH(NOW())
+        ");
+
+        $array = array();
+        while($exp = tep_db_fetch_array($queryExpiryday)){
+            $array[] = $exp;
+        }
+
+        return array(
+            data => array(
+                elements => $array
+            )
+        );
+
     }
 
 
--- adminSingaProperty/js/ng/app/report_customer_expire/controller/report_customer_expire_ctrl.js
+++ adminSingaProperty/js/ng/app/report_customer_expire/controller/report_customer_expire_ctrl.js
@@ -2,72 +2,38 @@ app.controller(
     'report_customer_expire_ctrl', [
         '$scope'
         , 'Restful'
-        , '$location'
         , 'Services'
-        , 'alertify'
-        , function ($scope, Restful, $location, Services, $alertify){
+        , function ($scope, Restful, Services){
             $scope.service = new Services();
-            var params = {pagination: 'yes'};
-            var url = 'api/CustomerPlan/';
-            $scope.init = function(params){
-                Restful.get(url, params).success(function(data){
+            var url = 'api/ExpirePlan/';
+            $scope.init = function(){
+                $scope.plans = '';
+                Restful.get(url).success(function(data){
                     $scope.plans = data;
-                    $scope.totalItems = data.count;
+                    //console.log(data);
                 });
             };
-            $scope.init(params);
+            $scope.init();
 
-            $scope.updateStatus = function(params){
-                params.status == 1 ? params.status = 0 : params.status = 1;console.log(params);
-                var data = { status: params.status, customers_id: params.detail[0].id, plan: params.plan};
-                Restful.put(url + params.id, data).success(function(data){
-                    console.log(data);
-                    $scope.service.alertMessage('<strong>Complete: </strong> Update Status Success.');
-                });
+            $scope.print = function(){
+                if( !$scope.plans ){
+                    return $scope.service.alertMessage(
+                        'Warning:','Please Filter to Print.','warning'
+                    );
+                }
+                var divToPrint = document.getElementById("print");
+                console.log(divToPrint);
+                var newWin= window.open("");
+                newWin.document.write('' +
+                    '<html><head>' +
+                    '<link href="css/print_table.css" rel="stylesheet" type="text/css">' +
+                    '</head>' +
+                    '<body>' + divToPrint.innerHTML + '</body>' +
+                    '</html>'
+                );
+                newWin.print();
+                newWin.close();
             };
 
-            // remove functionality
-            $scope.remove = function(id, $index){
-                $scope.id = id;
-                $scope.index = $index;
-
-                $alertify.okBtn("Ok")
-                    .cancelBtn("Cancel")
-                    .confirm("Are you sure want to delete this record?", function (ev) {
-                        // The click event is in the
-                        // event variable, so you can use
-                        // it here.
-                        ev.preventDefault();
-                        Restful.delete( url + $scope.id ).success(function(data){
-                            $scope.disabled = true;
-                            $scope.service.alertMessage('<strong>Complete: </strong>Delete Success.');
-                            $scope.init(params);
-                            //$scope.news.elements.splice($scope.index, 1);
-                            //$('#message').modal('hide');
-                        });
-                    }, function(ev) {
-                        // The click event is in the
-                        // event variable, so you can use
-                        // it here.
-                        ev.preventDefault();
-                    });
-
-            };
-            // search functionality
-            $scope.search = function(){
-                params.id = $scope.customer_id;
-                params.status = $scope.status;
-                $scope.init(params);
-            };
-            /**
-             * start functionality pagination
-             */
-            $scope.currentPage = 1;
-            //get another portions of data on page changed
-            $scope.pageChanged = function() {
-                $scope.pageSize = 10 * ( $scope.currentPage - 1 );
-                params.start = $scope.pageSize;
-                $scope.init(params);
-            };
         }
     ]);
